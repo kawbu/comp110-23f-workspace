6dc226f569d94eff96c469da6382903964872d0a
Kaw Bu <kawbu@unc.edu>
2023-11-16 01:07:05 -0500

    river simulation completed + jupyter notebook

--- exercises/ex08/bear.py
+++ exercises/ex08/bear.py
@@ -1,9 +1,21 @@
-"""File to define Bear class"""
+"""File to define Bear class."""
+
 
 class Bear:
+    """Class representing a Bear with attributes age and hunger_score."""
+    age: int
+    hunger_score: int
     
     def __init__(self):
-        return None
+        """Initializes bear object."""
+        self.age = 0
+        self.hunger_score = 0
     
     def one_day(self):
-        return None
+        """Adds one day to the current bear's age and decrements its hunger score by one."""
+        self.age += 1
+        self.hunger_score -= 1
+    
+    def eat(self, num_fish: int) -> None:
+        """Increments the current bear's hunger score by 3."""
+        self.hunger_score += num_fish
